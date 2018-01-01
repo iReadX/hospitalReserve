a829c25e99d81593c5a32ff0e677d16ccc544e07
Lurkerc <434848237@qq.com>
2018-01-01 23:08:55 +0800

    增加日志类型字段

--- util/keyMap.js
+++ util/keyMap.js
@@ -15,6 +15,13 @@ const logType = {
     userRegister: 'userRegister', // 用户注册
     userCheckPassWord: 'userCheckPassWord', // 用户修改密码
     userUpdateInfo: 'userUpdateInfo', // 用户更新信息
+    total: 'total', // 统计
+    list: 'list', // 列表
+    insert: 'insert', // 插入数据
+    update: 'update', // 更新数据
+    check: 'check', // 检测数据
+    select: 'select', // 数据查询
+    delete: 'delete', // 删除数据
 };
 
 module.exports = {
